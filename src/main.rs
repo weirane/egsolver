0400a74b84c8f975295c9dee5dc2bd08c6a24d41
Wang Ruochen <wrc@ruo-chen.wang>
2022-02-14 19:23:50 -0800

    Add skeleton of syntax file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod parse;
+
+use egg::*;
+use pest::Parser;
+
+use crate::parse::{Rule, SyGuSParser};
+
+define_language! {
+    enum BitVector {
+        "&" = And([Id; 2]),
+        "|" = Or([Id; 2]),
+        "^" = Xor([Id; 2]),
+        "~" = Not(Id),
+        Lit(i32),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut eg: EGraph<BitVector, ()> = Default::default();
+    let one = eg.add(BitVector::Lit(1));
+    eg.add(BitVector::And([one, one]));
+    println!("{:?}", eg);
+    // eg.dot().to_svg("egraph.svg").unwrap();
+
+    let p = SyGuSParser::parse(Rule::literal, "10.23").unwrap();
+    dbg!(p);
 }
---
+++ src/parse.rs
@@ -0,0 +1,5 @@
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "./sygus.pest"]
+pub struct SyGuSParser;
